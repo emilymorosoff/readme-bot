5da0de801ef77567b4e0f7d1e9fc072cda3c3c0c
emilymorosoff <emilymorosoff@icloud.com>
2023-12-02 20:46:28 -0800

    Enhanced README generation to include comprehensive sections and user input.

--- index.js
+++ index.js
@@ -9,7 +9,47 @@ const questions = [
         name: 'title',
         message: 'Project title:'
     },
-    // Add additional questions here
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Test instructions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD', 'None']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email address:'
+    }
 ];
 
 // Write README file
@@ -25,8 +65,42 @@ function writeToFile(fileName, data) {
 
 // Generate README content
 function generateReadmeContent(answers) {
-    // Use answers to construct README content
-    return `# ${answers.title}\n// Additional content here`;
+    const licenseBadge = answers.license !== 'None' ? `![License](https://img.shields.io/badge/license-${answers.license}-blue.svg)` : '';
+    const licenseNotice = answers.license !== 'None' ? `This project is licensed under the ${answers.license} license.` : 'This project is not licensed.';
+
+    return `# ${answers.title}
+
+${licenseBadge}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## License
+${licenseNotice}
+
+## Questions
+For any questions, please contact me at [${answers.email}](mailto:${answers.email}). Also, you can find more of my work at [${answers.github}](https://github.com/${answers.github}/).`;
 }
 
 // Initialize application
