c1e3d18b2a8d47c9f14b67120efd0ee8d1181f88
emilymorosoff <emilymorosoff@icloud.com>
2023-12-02 20:14:21 -0800

    Implemented README generation with user prompts.

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-// TODO: Include packages needed for this application
+// Required packages
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project title:'
+    },
+    // Add additional questions here
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('README.md created.');
+        }
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Generate README content
+function generateReadmeContent(answers) {
+    // Use answers to construct README content
+    return `# ${answers.title}\n// Additional content here`;
+}
 
-// Function call to initialize app
+// Initialize application
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            const readmeContent = generateReadmeContent(answers);
+            writeToFile('README.md', readmeContent);
+        });
+}
+
+// Start application
 init();
